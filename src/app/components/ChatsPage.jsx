9bede1f51f73b76f4abd096ed7012eb79aa17a95
ahmadsidikrofi <rofidragon71@gmail.com>
2024-02-11 20:09:14 +0700

    memberikan inisial yang login

--- src/app/components/ChatsPage.jsx
+++ src/app/components/ChatsPage.jsx
@@ -9,8 +9,11 @@ const ChatsPage = () => {
     }
     return (
         <>
-            <button onClick={handleLogout} className="p-3 bg-rose-600 w-full text-white font-medium">Logout</button>
-            <div className="background h-[100vh] w-full" style={{ fontFamily: 'quicksand' }}>
+            <div className="flex gap-1 p-2 bg-[#282b36]  text-white">
+                <div className="bg-rose-500 px-3 py-3 rounded-[100%] text-xl font-bold "> {storedUser.substring(0,2).toUpperCase()} </div>
+                <button onClick={handleLogout} className="p-3 bg-rose-600 w-full rounded-[15px]  font-medium">Logout</button>
+            </div>
+            <div className="background w-full" style={{ fontFamily: 'quicksand' }}>
                 <PrettyChatWindow 
                     projectId="4bd8149f-c578-4029-b452-d8ee9b40cf7d"
                     username={storedUser}
